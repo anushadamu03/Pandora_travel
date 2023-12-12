9583d4f4f84429f8c267348adcc67bcf897fccbb
anushadamu03 <76877073+anushadamu03@users.noreply.github.com>
2023-12-12 15:22:33 -0500

    Create AdditionCalculator.java

---
+++ AdditionCalculator.java
@@ -0,0 +1,10 @@
+// AdditionCalculator.java (Feature branch for addition)
+
+public class AdditionCalculator implements Calculator {
+    @Override
+    public double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    // Implement other methods from the Calculator interface
+}
