42ceac6d3807c897f22b8ac0db2c8605ccc642d8
anushadamu03 <76877073+anushadamu03@users.noreply.github.com>
2023-12-12 15:13:49 -0500

    Create calculator.java

---
+++ calculator.java
@@ -0,0 +1,8 @@
+// Calculator.java (Interface)
+
+public interface Calculator {
+    double add(double num1, double num2);
+    double subtract(double num1, double num2);
+    double multiply(double num1, double num2);
+    double divide(double num1, double num2) throws ArithmeticException;
+}
