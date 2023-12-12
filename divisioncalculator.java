9b24d36df7d077712361fab915779a245130cfe0
anush <anushadamu03@gmail.com>
2023-12-12 15:32:16 -0500

    adding division file

---
+++ divisioncalculator.java
@@ -0,0 +1,11 @@
+// divisionCalculator.java (Feature branch for division)
+
+public class divisionCalculator implements Calculator {
+    @Override
+    public double div(double num1, double num2) {
+        return num1 / num2;
+    }
+
+    
+}
+
