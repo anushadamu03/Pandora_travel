0d429106b0bb655620ee07ca17a640460966ec7e
anush <anushadamu03@gmail.com>
2023-12-12 15:43:52 -0500

    adding multiplication file

---
+++ multiplicationcalcultor.java
@@ -0,0 +1,11 @@
+// multiplicationCalculator.java (Feature branch for multiply)
+
+public class MultiplicationCalculator implements Calculator {
+    @Override
+    public double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    // You can also implement other methods from the Calculator interface if needed
+}
+
